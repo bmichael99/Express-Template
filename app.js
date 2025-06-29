0bb01769969eb9a2a945c79a6aee0aedd7405c1f
Michael <michaelbrandmeyer99@gmail.com>
2025-06-29 14:46:45 -0700

    integrated passport

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const { body, validationResult } = require("express-validator");
+const pool = require("./db/pool");
+const session = require("express-session");
+const passport = require("passport");
+const bcrypt = require("bcryptjs")
+const LocalStrategy = require('passport-local').Strategy;
+const pgSession = require('connect-pg-simple')(session);
+require('dotenv').config();
+
 
 //imports the express framework
 const express = require("express");
@@ -20,6 +28,72 @@ app.use(express.static(assetsPath));
 
 //parse form data into req.body
 app.use(express.urlencoded({ extended: true }));
+
+
+/**
+ *  -------------------- PASSPORT SETUP --------------------
+ */
+
+const sessionStore = new pgSession({
+    pool : pool,                // Connection pool
+    createTableIfMissing : true,
+    // Insert other connect-pg-simple options here
+  });
+
+//passport setup
+app.use(session({
+  store: sessionStore,
+  secret: process.env.SESSION_PASSWORD,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 *24
+  },
+ }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        // passwords do not match!
+        return done(null, false, { message: "Incorrect password" })
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
+
+/**
+ *  -------------------- ROUTER AND SERVER --------------------
+ */
 
 //serve index router when root is visited
 const indexRouter = require("./routes/indexRouter");
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const indexController = require("../controllers/indexController");
 const indexRouter = Router();
+const { isAuth } = require("../controllers/authMiddleware");
 
 
 indexRouter.get("/", indexController.showHomePage);
